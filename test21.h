dfecfcaff25dd275a05388ec57548dc3d897101d
fenghe-li <83200296+lifenghe-code@users.noreply.github.com>
2022-02-13 13:50:00 +0800

    合成两个有序链表

---
+++ test21.h
@@ -0,0 +1,67 @@
+//
+// Created by lifenghe on 2022/2/12.
+//
+
+#ifndef CLION_TEST21_H
+#define CLION_TEST21_H
+
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ **/
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution21 {
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        if (list1 == NULL || list2 == NULL) {
+            if (list1 != NULL) return list1;
+            else return list2;
+        }
+        ListNode *head = new ListNode(0, NULL);
+        ListNode *Head = new ListNode(0, NULL);
+        Head = head;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                head->next = list1;
+                head = list1;
+                list1 = list1->next;
+            } else if (list1->val > list2->val) {
+                head->next = list2;
+                head = list2;
+                list2 = list2->next;
+            } else {
+                head->next = list1;
+                head = list1;
+                list1 = list1->next;
+                head->next = list2;
+                head = list2;
+                list2 = list2->next;
+            }
+        }
+        if (list1 == NULL && list2 != NULL) {
+            head->next = list2;
+        } else if (list2 == NULL && list1 != NULL) {
+            head->next = list1;
+        } else{
+
+        }
+        return Head->next;
+    }
+};
+
+#endif //CLION_TEST21_H
